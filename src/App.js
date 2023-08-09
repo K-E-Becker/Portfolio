2c658df8a3138244736b3be29b167e5132fdeded
K-E_Becker <krstn.becker@gmail.com>
2023-08-09 19:44:46 -0400

    tech carousel

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           className="lottie"
         />
       </div>
-
+      <hr></hr>
       <Element name="about">
         <Parallax bgImage="" strength={500}>
           <div style={{ height: "500px" }}>
@@ -55,10 +55,60 @@ function App() {
 
       <Element name="tech">
         <Parallax bgImage="" strength={500}>
-          <div style={{ height: "500px" }}>This is for the tech stack</div>
+          <div style={{ height: "200px" }}>
+            Tech stack
+            <div className="techRowContainer">
+              <div className="techContainer">
+                <img src="html.png" height="100" width="100" alt="html" />
+                <img src="css.png" height="100" width="100" alt="css" />
+                <img src="js.png" height="100" width="100" alt="javascript" />
+                <img src="react.png" height="100" width="100" alt="react" />
+                <img src="redux.png" height="100" width="100" alt="redux" />
+                <img src="node.png" height="100" width="100" alt="node" />
+                <img src="pg.png" height="100" width="100" alt="pg" />
+                <img src="sql.png" height="100" width="100" alt="sql" />
+                <img src="mui.png" height="100" width="100" alt="mui" />
+                <img
+                  src="bootstrap.png"
+                  height="100"
+                  width="100"
+                  alt="bootstrap"
+                />
+                <img
+                  src="firebase.png"
+                  height="100"
+                  width="100"
+                  alt="firebase"
+                />
+              </div>
+              <div className="techContainer">
+                <img src="html.png" height="100" width="100" alt="html" />
+                <img src="css.png" height="100" width="100" alt="css" />
+                <img src="js.png" height="100" width="100" alt="javascript" />
+                <img src="react.png" height="100" width="100" alt="react" />
+                <img src="redux.png" height="100" width="100" alt="redux" />
+                <img src="node.png" height="100" width="100" alt="node" />
+                <img src="pg.png" height="100" width="100" alt="pg" />
+                <img src="sql.png" height="100" width="100" alt="sql" />
+                <img src="mui.png" height="100" width="100" alt="mui" />
+                <img
+                  src="bootstrap.png"
+                  height="100"
+                  width="100"
+                  alt="bootstrap"
+                />
+                <img
+                  src="firebase.png"
+                  height="100"
+                  width="100"
+                  alt="firebase"
+                />
+              </div>
+            </div>
+          </div>
         </Parallax>
       </Element>
-
+      <hr></hr>
       <Element name="contact">
         <Parallax bgImage="" strength={500}>
           <div style={{ height: "500px" }}>
